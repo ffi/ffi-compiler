d27e1b598d90244562118db28032c631c8aa9a9a
Wayne Meissner <wmeissner@gmail.com>
2013-01-21 13:30:33 +1100

    Fix up gemspec and implement functions

--- example/ext/example.c
+++ example/ext/example.c
@@ -5,3 +5,15 @@ example(void)
 {
   return 0xdeadbeef;
 }
+
+RBFFI_EXPORT int 
+foo(struct Example_Foo *foo)
+{
+  return 0;
+}
+
+RBFFI_EXPORT int 
+bar(struct Example_Bar bar, struct Example_Foo *foo)
+{
+  return 0;
+}

dd5efae52d081827377c45a98b9612c4dc85c724
Hugo Frappier <hugo@cekoya.com>
2013-04-13 16:36:13 -0400

    Properly determine caller's dirname on Windows
    The path includes the drive letter followed by :, which is the separator
    we use to split the path.

--- lib/ffi-compiler/loader.rb
+++ lib/ffi-compiler/loader.rb
@@ -8,7 +8,7 @@ module FFI
       def self.find(name, start_path = nil)
         library = Platform.system.map_library_name(name)
         root = false
-        Pathname.new(start_path || File.dirname(caller[0].split(/:/)[0])).ascend do |path|
+        Pathname.new(start_path || caller_path(caller[0])).ascend do |path|
           Dir.glob("#{path}/**/{#{FFI::Platform::ARCH}-#{FFI::Platform::OS}/#{library},#{library}}") do |f|
             return f
           end
@@ -19,6 +19,17 @@ module FFI
           root = File.basename(path) == 'lib'
         end
         raise LoadError.new("cannot find '#{name}' library")
+      end
+
+      def self.caller_path(line = caller[0])
+        if FFI::Platform::OS == 'windows'
+          drive = line[0..1]
+          path =  line[2..-1].split(/:/)[0]
+          full_path = drive + path
+        else
+          full_path = line.split(/:/)[0]
+        end
+        File.dirname full_path
       end
     end
   end
